4f9afd40763c7f6987248202cfc03a941830a490
Pierre CAZENAVE <pierre.cazenave33@outlook.fr>
2024-02-20 14:37:39 +0100

    add args

--- C_export/main.cpp
+++ C_export/main.cpp
@@ -21,98 +21,50 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    // BMP bitmap;
-    // FILE *pFichier = NULL;
-    // char filename[] = "../bmp_traitee/1_0.bmp";
-    // pFichier = fopen(filename, "rb"); // Ouverture du fichier contenant l'image
-    // if (pFichier == NULL)
-    // {
-    //     printf("%s\n", filename);
-    //     printf("Erreur dans la lecture du fichier\n");
-    // }
-    // LireBitmap(pFichier, &bitmap);
-    // fclose(pFichier); // Fermeture du fichier contenant l'image
+    int chiffreDebut = 0, chiffreFin = 9;
+    int quantiteDebut = 0, quantiteFin = 10;
+    char nomFichierSource[200] = "../bmp_traitee/";
 
-    // ConvertRGB2Gray(&bitmap);
-    // printf("%d\n", bitmap.mPixelsGray[10][10]);
-
-    // printf("aaaa\r\n");
-    if (argc > 1)
+    if (argc > 2)
     {
         if (strstr(argv[1], "convert"))
         {
             BMP bitmap_traitement;
-            TraitementDonnees(&bitmap_traitement, "../../Database/Prof/bmpProcessedSeuil/", "../bmp_traitee/", 0, 9, 0, 20, (unsigned char)(255 / 2.f), 0, 1, 28, 28);
+            chiffreDebut = atoi(argv[4]);
+            chiffreFin = atoi(argv[5]);
+            quantiteDebut = atoi(argv[6]);
+            quantiteFin = atoi(argv[7]);
+            TraitementDonnees(&bitmap_traitement, argv[2], argv[3], chiffreDebut, chiffreFin, quantiteDebut, quantiteFin, (unsigned char)(255 / 2.f), 0, 1, 28, 28);
+            exit(0);
+        }
+        else if (strstr(argv[1], "infer") && argc > 3)
+        {
+            strncpy(nomFichierSource, argv[2], 200);
+            chiffreDebut = atoi(argv[3]);
+            chiffreFin = atoi(argv[4]);
+            quantiteDebut = atoi(argv[5]);
+            quantiteFin = atoi(argv[6]);
         }
         else
+        {
+            printf("Usage: infer||dossier||chiffreDebut||chiffreFin||quantiteDebut||quantiteFin\r\n");
+            printf("Usage: convert||source||dest||chiffreDebut||chiffreFin||quantiteDebut||quantiteFin\r\n");
             printf("argc=%d; argv[0]=\"%s\"; argv[1]=\"%s\";", argc, argv[0], argv[1]);
+        }
+    }
+    else
+    {
+        printf("Not enough args\r\n");
+        exit(1);
     }
 
-    // int im_size = bitmap.infoHeader.hauteur * bitmap.infoHeader.largeur;
-    // printf("bitmap(l/h):%d/%d;im_size=%d\n", bitmap.infoHeader.largeur, bitmap.infoHeader.hauteur, im_size);
-    // uint8_t *bitmap_div = (uint8_t *)malloc(im_size * sizeof(uint8_t));
-    // int width, height;
-    // width = bitmap.infoHeader.largeur;
-    // height = bitmap.infoHeader.hauteur;
-    // for (int col = 0; col < width; col++)
-    // {
-    //     for (int row = 0; row < height; row++)
-    //     {
-    //         bitmap_div[width * row + col] = bitmap.mPixelsGray[row][col];
-    //     }
-    // }
-
-    // //  Use the correct scaling, i.e., low and high.
-    // const auto input = fdeep::tensor_from_bytes(bitmap_div,
-    //                                             static_cast<std::size_t>(height),
-    //                                             static_cast<std::size_t>(width),
-    //                                             static_cast<std::size_t>(1),
-    //                                             0.0f, 1.0f);
-
     const auto model = fdeep::load_model("fdeep_model.json");
-    // const auto result = model.predict({input});
 
-    // float results_float[10];
-    // printf("aaaa\r\n");
-    // sscanf(fdeep::show_tensors(result).c_str(),"[[[[[[%f, %f, %f, %f, %f, %f, %f, %f, %f, %f]]]]]]",&results_float[0],&results_float[1],&results_float[2],&results_float[3],&results_float[4],&results_float[5],&results_float[6],&results_float[7],&results_float[8],&results_float[9]);
-    // float local_max;
-    // int i_local_max;
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     printf("for:%d value=%f\r\n",i,results_float[i]);
-    //     float a = results_float[i];
-    //     if (a > local_max)
-    //     {
-    //         local_max = a;
-    //         i_local_max = i;
-    //     }
-    // }
-    // printf("max[%d]=%f\r\n", i_local_max, local_max);
-    // std::cout << fdeep::show_tensors(result) << std::endl;
-
-    // BMP bitmap;
-    // FILE *pFichier = NULL;
-    // char filename[] = "../bmp_traitee/0_0.bmp";
-    // pFichier = fopen(filename, "rb"); // Ouverture du fichier contenant l'image
-    // if (pFichier == NULL)
-    // {
-    //     printf("%s\n", filename);
-    //     printf("Erreur dans la lecture du fichier\n");
-    //     while (1)
-    //         ;
-    // }
-    // LireBitmap(pFichier, &bitmap);
-    // fclose(pFichier); // Fermeture du fichier contenant l'image
-
-#if 1
     int output_size = 10;
     int error = 0;
     int loop_count = 0;
     FILE *pFichier;
     BMP bitmap;
-    int chiffreDebut = 0, chiffreFin = 9;
-    int quantiteDebut = 0, quantiteFin = 10;
-    char nomFichierSource[200] = "../bmp_traitee/";
     char filename[200] = {0};
     char nom[200];
 
@@ -206,7 +158,7 @@ int main(int argc, char *argv[])
             free(bitmap_div);
         }
     }
-#endif
+
     printf("error count=%d/%d\r\n", error, loop_count);
     return 0;
 }
